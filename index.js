8d393d8b5d064e3bdc827cdde6cc9ac5087bd7bf
Wesley <wes.beck21@outlook.com>
2021-02-19 11:45:16 -0800

    commit tree ef48273616b091a54ef5854e84b91be566945d32

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code here
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+} 
+
+function moveDodgerRight() {
+  let rightNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(rightNumbers, 10) 
+
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+}); 
